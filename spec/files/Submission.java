c98bd452e92b50e4aa1b1db2484f99eb5426b1ef
Bryan Bibat <bry@bryanbibat.net>
2012-11-29 17:52:41 +0800

    Change formatting for Java

--- spec/files/Submission.java
+++ spec/files/Submission.java
@@ -6,20 +6,20 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Submission {
-	public static void main(String... args) throws IOException {
-		Scanner sc = new Scanner(new File("input.txt"));
-		PrintWriter out
-		   = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
-	    while (sc.hasNextLine()) {
-	        Scanner lsc = new Scanner(sc.nextLine());
-	        long sum = 0;
-	        while (lsc.hasNextInt()) {
-	        	sum += lsc.nextInt();	      
-	        }
-	        out.println(Long.toString(sum));
-	        lsc.close();
-	    }
-	    out.close();
-	    sc.close();
-	}
+    public static void main(String... args) throws IOException {
+        Scanner sc = new Scanner(new File("input.txt"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+                "output.txt")));
+        while (sc.hasNextLine()) {
+            Scanner lsc = new Scanner(sc.nextLine());
+            long sum = 0;
+            while (lsc.hasNextInt()) {
+                sum += lsc.nextInt();
+            }
+            out.println(Long.toString(sum));
+            lsc.close();
+        }
+        out.close();
+        sc.close();
+    }
 }
--- spec/files/SubmissionError.java
+++ spec/files/SubmissionError.java
@@ -5,21 +5,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class SubmissionError {
-	public static void main(String... args) throws IOException {
-		Scanner sc = new Scaner(new File("input.txt"));
-		PrintWriter out
-		   = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
-	    while (sc.hasNextLine()) {
-	        Scanner lsc = new Scanner(sc.nextLine());
-	        long sum = 0;
-	        while (lsc.hasNextInt()) {
-	        	sum += lsc.nextInt();	      
-	        }
-	        out.println(Long.toString(sum));
-	        lsc.close();
-	    }
-	    out.close();
-	    sc.close();
-	}
+public class SubmissionJava {
+    public static void main(String... args) throws IOException {
+        Scanner sc = new Scaner(new File("input.txt"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+                "output.txt")));
+        while (sc.hasNextLine()) {
+            Scanner lsc = new Scanner(sc.nextLine());
+            long sum = 0;
+            while (lsc.hasNextInt()) {
+                sum += lsc.nextInt();
+            }
+            out.println(Long.toString(sum));
+            lsc.close();
+        }
+        out.close();
+        sc.close();
+    }
 }
+
